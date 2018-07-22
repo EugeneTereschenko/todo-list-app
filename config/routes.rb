928991daff480567bcb067c75bfcce4e55153e3d
EugeneTereschenko <tereschenko.eugene@gmail.com>
2018-07-22 13:54:33 +0300

    config devise settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    
-    
     root to: 'index#index'
-    
     resources :projects do
         resources :tasks do
                 get 'up'  => 'tasks#up'
                 get 'down'  => 'tasks#down'
         end
     end
-        
     resources :users
-    
 end
